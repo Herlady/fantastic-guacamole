cdaf15815494de9c6ca9729f9d90c477930acd2a
Herlady <97959378+Herlady@users.noreply.github.com>
2022-05-30 13:57:27 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c, answer;
+    cout << "Enter the first number" << endl;
+    cin >> a;
+    cout<< "Enter second number";
+    cin >> b;
+    cout<< "Enter the third number";
+    cin >> c;
+    answer = a*b*c;
+    cout<< "product of the three number"<< answer;
+
+    return 0;
+}
